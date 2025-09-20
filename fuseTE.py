64c823bfcdfa3a28c385aab651ba964a70d9a525
Pelle Scholten <pellescholten@MacBook-Pro-van-Pelle.local>
2025-09-20 19:19:44 +0200

    streamling labelling of annotations to be merged

--- fuseTE.py
+++ fuseTE.py
@@ -4,6 +4,15 @@ import sys
 from collections import defaultdict
 nested_dict = lambda: defaultdict(nested_dict)
 
+d = dict()
+def update_dictionary(d, col, cattrD, grouped):
+	d[col[0]][cattrD["ID"]]["lastcol"] = col
+	d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
+	d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
+	d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
+	d[col[0]][cattrD["ID"]]["grouped"] = grouped
+	return d
+
 def truefusete(gffp,gapsize,outfile, mergemode):
 
 	gff = str(gffp)
@@ -16,30 +25,27 @@ def truefusete(gffp,gapsize,outfile, mergemode):
 
 	# quick check number of line of the file
 	sh = subprocess.run(['wc', '-l',gff], stdout=subprocess.PIPE)
-	totalline = str(int(sh.stdout.split()[0]))
+	totalline = int(sh.stdout.split()[0])
 
-	cnt = 0
 	d = nested_dict()
-	lastchrom = ""
+	lastchrom = "" 
 
 	# Progress
 	dcnt = 0
-
 	sys.stderr.write("\rLabelling fragmented elements (for alignment)...\n")
 
 	print("##gff-version 3")
-	print("##repeatcraft")
+	print("##TEatree labeled")
 
 	with open(gff, "r") as f:
 		for line in f:
 			if line.startswith("#"):
 				continue
+
 			# Progress
 			dcnt += 1
-
-			
-			#if dcnt % 1000 == 0:
-			#	sys.stderr.write("\rProgress:" + str(dcnt) + "/"+ totalline+ "...\n")
+			if dcnt % 10000 == 0 or dcnt == totalline:
+				sys.stderr.write("\rProgress: " + str(dcnt) + "/"+ str(totalline)+ " ...\n")
 
 			col = line.rstrip().split("\t")
 
@@ -52,158 +58,139 @@ def truefusete(gffp,gapsize,outfile, mergemode):
 			cattrD["Tstart"] = int(cattrD["Tstart"])
 			cattrD["Tend"] = int(cattrD["Tend"])
 
-			# if changing to the last column, need to print the what havn't print out (lastcol for all families in last chrom)
+			# if changing to the last column, need to print the what have not printed  (lastcol for all families in last chrom)
 			if  col[0] != lastchrom:
 				if lastchrom != "":
-					#print("new chrom, print remaining lastcol") # debug
 					for family in d[lastchrom]:
 						print(*d[lastchrom][family]["lastcol"],sep="\t")
 
 			lastchrom = col[0] # Update lastchrom
+
+			if not d[col[0]][cattrD["ID"]]:
+				# first of family on this chrom, cannot merge, just add to dictionary
+				d = update_dictionary(d, col, cattrD, False)
+				continue
+
+			# check 
+			if mergemode == "threshold":
+				match = (int(col[3]) - d[col[0]][cattrD["ID"]]["lastend"]) <= gapsize
+			elif mergemode == "ID":
+				match = d[col[0]][cattrD["ID"]]["lastcol"][9] == col[9]
+			else:
+				match = d[col[0]][cattrD["ID"]]["lastcol"][9] == col[9] or (int(col[3]) - d[col[0]][cattrD["ID"]]["lastend"]) <= gapsize
 		
+			if not match:
+				# no need to group the two records
+				# print the last record of the last family group without adding new label, and update dictionary
+				col2print = d[col[0]][cattrD["ID"]]["lastcol"]
+				print(*col2print, sep = "\t")
+				d = update_dictionary(d, col, cattrD, False)
+				continue
 
-			if d[col[0]][cattrD["ID"]]:  # not the first of this family on this chrom
-				#print("not first family") # debug
-
-				#if col[0] != d[col[0]][cattrD["ID"]]["lastcol"][0] # new chromosome, maybe unecessary
-	
-				if mergemode == "threshold":
-					match = (int(col[3]) - d[col[0]][cattrD["ID"]]["lastend"]) <= gapsize
-				elif mergemode == "ID":
-					match = d[col[0]][cattrD["ID"]]["lastcol"][9] == col[9]
-				else:
-					threshold = (int(col[3]) - d[col[0]][cattrD["ID"]]["lastend"]) <= gapsize
-					idmatch = d[col[0]][cattrD["ID"]]["lastcol"][9] == col[9]
-					match = threshold or idmatch
+			# Is the previous annotation already grouped?
+			if not d[col[0]][cattrD["ID"]]["grouped"]: 
+				# the lastcol is the first element is this group, just need to check if last and current copies overlap
 				
-				if not match: # or col[0] != d[col[0]][cattrD["ID"]]["lastcol"][0]:
-					#print("larger than 150") # debug
-					# don't need to group the two records
-					# print the lastest record of the lastest family group without adding new label
-					col2print = d[col[0]][cattrD["ID"]]["lastcol"]
-					print(*col2print, sep = "\t")
-					# update the dictionary
-					d[col[0]][cattrD["ID"]]["lastcol"] = col
-					d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
-					d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
-					d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
-					d[col[0]][cattrD["ID"]]["grouped"] = False
-				else:
-					#print("less than 150") # debug
-					# Is the lastcol carrying a TEgroup label?
-					if d[col[0]][cattrD["ID"]]["grouped"]:
-						#print("last one have label") # debug
-						# check consensus information (all in last group)
-						groupnumber = d[col[0]][cattrD["ID"]]["groupcnt"]
-						o = False
-						for i in range(cattrD["Tstart"],cattrD["Tend"]):
-							if i in list(d[col[0]][cattrD["ID"]][groupnumber]):
-								o = True
-								break
-						if o: # overlap with some copies in the group, break the grouping
-							#print("consensus overlap") # debug
-							print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
-							d[col[0]][cattrD["ID"]]["lastcol"] = col
-							d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
-							d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
-							d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
-							d[col[0]][cattrD["ID"]]["grouped"] = False #????
-						else:
-							#print("consensus pass") # debug
-							# print the lastcol directly
-							print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
-							# Update consensus coverage
-							for i in range(cattrD["Tstart"],cattrD["Tend"]):
-								d[col[0]][cattrD["ID"]][groupnumber][i] = 1
+				#FIX
+				# is there really no overlap allowed at all??
+				# perhaps can be a bit more lenient?
 
-							# Update last col using label from last label
-							attr = ";TEgroup=" + col[0] + "|" + cattrD["ID"] + "|" + str(d[col[0]][cattrD["ID"]]["groupcnt"])
-							col[8] = col[8] + attr
-							d[col[0]][cattrD["ID"]]["lastcol"] = col
-							d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
-							d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
-							d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
-							d[col[0]][cattrD["ID"]]["grouped"] = True
+				#check whether strandedness is readable
+				if col[6] != "-" and col[6] != "C" and col[6] != "+":
+					sys.stderr.write("\r Error, unknown strandedness while labeling annotations to be merged (\'+\', \'-\' or \'C\' expected)\n\r"+col[6]+"\n")
+					sys.exit(1)
+
+				# check whether strandness is the same of the annotations a.k.a. Is the orientation of the annotation the same
+				if d[col[0]][cattrD["ID"]]["lastcol"][6] != col[6]:
+					print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
+					d = update_dictionary(d, col, cattrD, False)
+					continue
+
+				#check whether annotation on consensus overlap
+				c_start = int(d[col[0]][cattrD["ID"]]["Tstart"])
+				c_end = int(d[col[0]][cattrD["ID"]]["Tend"])
+				if c_start <= int(cattrD["Tstart"]) <= c_end or c_start <= int(cattrD["Tend"]) <= c_end or int(cattrD["Tstart"]) <= c_start <= int(cattrD["Tend"]) or int(cattrD["Tstart"]) <= c_end <= int(cattrD["Tend"]):
+					print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
+					d = update_dictionary(d, col, cattrD, False)
+					continue
+
+				# passed all tests
+				# label for merger, first merger of group
+				# can open a new group now and update the attr of the last and current copies
+
+				# Make a new label for lastcol and current col
+				d[col[0]][cattrD["ID"]]["groupnumber"] = d[col[0]][cattrD["ID"]].get("groupnumber", 0) + 1
+
+				# Mark down the consensus coverage
+				# fix this, inefficient, do ranges instead of individual positions
+				groupnumber = d[col[0]][cattrD["ID"]]["groupnumber"]
+
+				#write down consensus coverage
+				#make a list of ranges that, one range for each of the two grouped annotations
+				d[col[0]][cattrD["ID"]][groupnumber]["consensus_coverage"] = [(d[col[0]][cattrD["ID"]]["Tstart"],d[col[0]][cattrD["ID"]]["Tend"]), (cattrD["Tstart"],cattrD["Tend"])]
+
+				# Print lastcol and update dictionary
+				lastcol2print = d[col[0]][cattrD["ID"]]["lastcol"]
+				attr = ";TEgroup=" + col[0] + "|" + cattrD["ID"] + "|" + str(d[col[0]][cattrD["ID"]]["groupnumber"])
+				lastcol2print[8] = lastcol2print[8] + attr
+				print(*lastcol2print,sep="\t")
+				col[8] = col[8] + attr # Update lastcol
+				d = update_dictionary(d, col, cattrD, True)
+
+			else: # previous annotation of this family is already grouped
+				# check consensus information (all in last group)
+				groupnumber = d[col[0]][cattrD["ID"]]["groupnumber"]
+				overlap = False
+
+				#FIX
+				#do not check all positions, inefficient
+				#check list of ranges instead, similar to check later
+				#also, idem to later, do I need to be that strict?
+				#do i also need to check strandedness etc here? i think so?
 
 
-					else: # the lastcol is the first element is this group, just need to check if last and current copies overlap
-						#print("last copy no label") # debug
+				#check whether strandedness is readable
+				if col[6] != "-" and col[6] != "C" and col[6] != "+":
+					sys.stderr.write("\r Error, unknown strandedness while labeling annotations to be merged (\'+\', \'-\' or \'C\' expected)\n\r"+col[6]+"\n")
+					sys.exit(1)
 
-						#if d[col[0]][cattrD["ID"]]["Tstart"] >= cattrD["Tstart"]:
-						#	o = cattrD["Tend"] - d[col[0]][cattrD["ID"]]["Tstart"]
-						#else:
-						#	o = d[col[0]][cattrD["ID"]]["Tend"] - cattrD["Tstart"]
-						#o = min(d[col[0]][cattrD["ID"]]["Tend"], cattrD["Tstart"]) - max(d[col[0]][cattrD["ID"]]["Tstart"], cattrD["Tend"]) #old
+				# check whether strandness is the same of the annotations a.k.a. Is the orientation of the annotation the same
+				if str(d[col[0]][cattrD["ID"]]["lastcol"][6]) != str(col[6]):
+					print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
+					d = update_dictionary(d, col, cattrD, False)
+					continue
 
-						strandedness_not_equal = d[col[0]][cattrD["ID"]]["lastcol"][6] != col[6]
-						if col[6] == "+":
-							if int(cattrD["Tstart"]) > int(d[col[0]][cattrD["ID"]]["Tend"]):
-								overlapping_consensus = False
-							else:
-								overlapping_consensus = True
-						elif col[6] == "-" or col[6] == "C":
-							if int(cattrD["Tend"]) < int(d[col[0]][cattrD["ID"]]["Tstart"]):
-								overlapping_consensus = False
-							else:
-								overlapping_consensus = True	
-						else:
-							sys.stderr.write("\r error,unknown strandedness\n")
-							sys.stderr.write("\r"+col[6]+"\n")
-							sys.exit(1)
+				#check whether annotation on consensusoverlaps 
+				for r in d[col[0]][cattrD["ID"]][groupnumber]["consensus_coverage"]:
+					c_start, c_end = r
+					if c_start <= int(cattrD["Tstart"]) <= c_end or c_start <= int(cattrD["Tend"]) <= c_end or int(cattrD["Tstart"]) <= c_start <= int(cattrD["Tend"]) or int(cattrD["Tstart"]) <= c_end <= int(cattrD["Tend"]):
+						overlap = True	
+						break
 
-						if overlapping_consensus or strandedness_not_equal:  # Consensus position overlap, don't need to group them just print
-							#print("consensus overlap") # debug
-							print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
-							d[col[0]][cattrD["ID"]]["lastcol"] = col
-							d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
-							d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
-							d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
-							d[col[0]][cattrD["ID"]]["grouped"] = False
+				if overlap: 
+					print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
+					d = update_dictionary(d, col, cattrD, False)
+					continue
 
-						else: # can open a new group now and update the attr of the last and current copies
-							#print("consensus pass") # debug
-							# Make a new label for lastcol and current col
-							if d[col[0]][cattrD["ID"]]["groupcnt"]: # Is there a previous family group in the same chrom?
-								d[col[0]][cattrD["ID"]]["groupcnt"] = d[col[0]][cattrD["ID"]]["groupcnt"] + 1
-							else:
-								d[col[0]][cattrD["ID"]]["groupcnt"] = 1
-
-							# Mark down the consensus coverage
-							groupnumber = d[col[0]][cattrD["ID"]]["groupcnt"]
-							for i in range(d[col[0]][cattrD["ID"]]["Tstart"],d[col[0]][cattrD["ID"]]["Tend"]):
-								d[col[0]][cattrD["ID"]][groupnumber][i] = 1
-							for i in range(cattrD["Tstart"],cattrD["Tend"]):
-								d[col[0]][cattrD["ID"]][groupnumber][i] = 1
-
-							# Print lastcol
-							lastcol2print = d[col[0]][cattrD["ID"]]["lastcol"]
-							attr = ";TEgroup=" + col[0] + "|" + cattrD["ID"] + "|" + str(d[col[0]][cattrD["ID"]]["groupcnt"])
-							lastcol2print[8] = lastcol2print[8] + attr
-							print(*lastcol2print,sep="\t")
-
-							# Update lastcol
-							col[8] = col[8] + attr
-							d[col[0]][cattrD["ID"]]["lastcol"] = col
-							d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
-							d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
-							d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
-							d[col[0]][cattrD["ID"]]["grouped"] = True
-
-			else: # first family on this chrom
-				#print("first element on this chrom") # debug
-				d[col[0]][cattrD["ID"]]["lastcol"] = col
-				d[col[0]][cattrD["ID"]]["lastend"] = int(col[4])
-				d[col[0]][cattrD["ID"]]["Tstart"] = cattrD["Tstart"]
-				d[col[0]][cattrD["ID"]]["Tend"] = cattrD["Tend"]
-				d[col[0]][cattrD["ID"]]["grouped"] = False
+				# passed all tests, and group already present
+				# add annotation to existing group
+				print(*d[col[0]][cattrD["ID"]]["lastcol"], sep="\t")
+				
+				# Update consensus coverage of grsys;oup
+				d[col[0]][cattrD["ID"]][groupnumber]["consensus_coverage"].append((cattrD["Tstart"],cattrD["Tend"]))
+				
+				# Update last col using label from last label
+				attr = ";TEgroup=" + col[0] + "|" + cattrD["ID"] + "|" + str(d[col[0]][cattrD["ID"]]["groupnumber"])
+				col[8] = col[8] + attr
+				d = update_dictionary(d, col, cattrD, True)
 
 		# print the last record for all families from the last chrom
-		#print("print remaining lastcol") # debug
 		for family in d[lastchrom]:
 			print(*d[lastchrom][family]["lastcol"],sep="\t")
 
 	sys.stdout.close()
 
+	#resort the outfile based on position in shell
 	c = "sort -k1,1 -k4,4n -k5,5n " + sort + " >" + outfile + "&& rm " + sort
 	subprocess.run(c, shell=True)
 	sys.stderr.write("\n")
